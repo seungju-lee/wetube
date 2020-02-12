b1711c9bc8698aff8f0a5bcf7051a27398eccb76
seungju-lee <warez200001@gmail.com>
2020-02-12 23:01:19 +0900

    Handling Routes with Express

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello');
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening(){
+    console.log(`LISTENING ON : HTTP://localhost:${PORT}`)
+}
+
+function handleHome(req, res){
+    console.log(req);
+    res.send("This is my First HomePage");
+}
+
+function handleProfile(req, res){
+    res.send("This is my Profile");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
