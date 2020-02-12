673f24de971d38aecbd23a3a4960fa7e149f28f0
seungju-lee <warez200001@gmail.com>
2020-02-13 00:10:33 +0900

    babel setup

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening(){
-    console.log(`LISTENING ON : HTTP://localhost:${PORT}`)
-}
+const handleListening = () => console.log(`LISTENING ON : HTTP://localhost:${PORT}`);
 
-function handleHome(req, res){
-    console.log(req);
-    res.send("This is my First HomePage");
-}
+const handleHome = (req, res) => res.send("This is my First HomePage");
 
-function handleProfile(req, res){
-    res.send("This is my Profile");
-}
+const handleProfile = (req, res) => res.send("This is my Profile");
 
 app.get("/", handleHome);
+
 app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
