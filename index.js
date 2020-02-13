3b45b6107180320d6f575d36a8aa004fc3bc35a1
seungju-lee <warez200001@gmail.com>
2020-02-13 22:58:52 +0900

    setup express enviroment

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -8,6 +13,13 @@ const handleListening = () => console.log(`LISTENING ON : HTTP://localhost:${POR
 const handleHome = (req, res) => res.send("This is my First HomePage");
 
 const handleProfile = (req, res) => res.send("This is my Profile");
+
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
